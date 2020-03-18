ccc33321272419c255b2b2a46c22ca27cdc2a75a
Jan MotÃak <jan.motak@hotmail.cz>
2020-03-18 12:30:25 +0100

    added images

--- mainFile.py
+++ mainFile.py
@@ -1,6 +1,7 @@
 import cv2
 import tensorflow as tf
 import os #itarate through directiories and join paths
+import matplotlib.pyplot as plt #show img
     
 CATEGORIES = ["Dog","Cat"]
 IMAGEDIR = "C:/Users/JanMo/Desktop/Skola/Neural networks/catsAndDogs/images"
@@ -15,6 +16,10 @@ def predictData():
     for img in os.listdir(IMAGEDIR):
         prediction = model.predict([prepare(os.path.join(IMAGEDIR,img))]) # always predict a list
         print("Prediction: " + CATEGORIES[int(prediction[0][0])] + " " + "Reality: " + str(img))
+        #img_array = cv2.imread(os.path.join(IMAGEDIR,img), cv2.IMREAD_GRAYSCALE)
+        #new_array = cv2.resize(img_array,(75,75))
+        #plt.imshow(new_array, cmap="gray")
+        #plt.show()
      
 model = tf.keras.models.load_model("64x3-CNN-new.model")
 
